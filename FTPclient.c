ca800e87b9737768a374bc448012e16370f7f38d
Nurgazy Seidaliev <nks369@nyu.edu>
2020-03-06 23:46:52 +0400

    Client updates

--- FTPclient.c
+++ FTPclient.c
@@ -14,8 +14,87 @@
 
 
 int main(int argc, char* argv[]) {
-    
-    
+    int server_port;
+    char server_addr[100];
+
+    if(argc != 3){
+        printf("Some arguments are missing, enter server address  and port number\n");
+        return 1;
+    }
+    else{
+        server_port = atoi(argv[2]);
+        strcpy(server_addr, argv[1]);
+        printf("Server's IP address: %s\n", server_addr);
+        printf("Server's port number: %d\n", server_port);
+    }
+    char buf[1024];// to send and receive messages
+    int sockfd;//Communication socket
+    struct sockaddr_in address;
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        printf("Cannot open socket \n");
+        exit(1);
+    }
+
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_port = htons(server_port);
+    inet_aton(server_addr, &(address.sin_addr));
+    if (connect(sockfd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        printf("Connection failed\n");
+        exit(1);
+    }
+    printf("Connected to the server\n");
+    while(1){
+        char *cmd;
+        while(1){
+            printf("ftp> ");
+            char in[1024];
+            fgets(buf,1024, stdin);
+            strcpy(in, buf);
+            cmd = strtok(in, "\n");
+            if(NULL == cmd){
+                continue;
+            }
+            cmd = strtok(cmd, " ");
+            if(NULL == cmd){
+                continue;
+            }
+            break;
+        }
+        
+        if((strcmp(cmd, "USER")) == 0){
+            printf(" buf:%s\n", buf);
+            write(sockfd, buf, strlen(1+buf));
+            memset(buf, 0, strlen(buf));
+            
+            if((strcmp(buf, "Username does not exist")) == 0){
+                printf("Username does not exist\n");
+            }
+            else if((strcmp(buf, "Username OK, password required")) == 0){
+                printf("Username OK, password required\n");
+            }
+            else{
+                printf("Incorrect response from server\n");
+            }
+        }
+        else if((strcmp(cmd, "PASS")) == 0){
+            write(sockfd, buf, strlen(1+buf));
+            memset(buf, 0, strlen(buf));
+            if((strcmp(buf, "wrong password")) == 0){
+                printf("wrong password\n");
+            }
+            else if((strcmp(buf, "Authentication complete")) == 0){
+                printf("Authentication complete\n");
+            }
+            else if((strcmp(buf, "set USER first")) == 0){
+                printf("set USER first\n");
+            }
+            else{
+                printf("Incorrect response from server\n");
+            }
+        }
+    }
+
 
 
     return 0;
